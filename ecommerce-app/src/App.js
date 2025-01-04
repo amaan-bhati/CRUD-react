1d97ffcf75ab449e2582ca9ae7c5719e62c014a1
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2025-01-04 12:56:08 +0000

    chore: add react router dom and set up  routes on the app file

--- ecommerce-app/src/App.js
+++ ecommerce-app/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Navbar from './components/Navbar';
-//todo : export routes from router dom here
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
